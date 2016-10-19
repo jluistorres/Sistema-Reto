4afc98fbd88446c52314c7cd9c5643160e9f87ae
jluistorres <jtorreszarate2405@gmail.com>
2016-10-19 17:45:47 -0500

    elmah

--- Reto/Controllers/ErrorController.cs
+++ Reto/Controllers/ErrorController.cs
@@ -7,7 +7,17 @@ using System.Web.Mvc;
 namespace Reto.Controllers
 {
     public class ErrorController : Controller
-    {       
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
         public ActionResult Restringido()
         {
             return View();
